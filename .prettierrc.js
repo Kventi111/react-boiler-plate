01c4091ac5e2e5476eaec6d141251a84c6f07a31
Babaev Magomed <Magomed.babaev@raiffeisen.ru>
2020-12-17 17:48:17 +0300

    update prettier

---
+++ .prettierrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    printWidth: 160,
+    proseWrap: 'never',
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: 'always',
+    overrides: [
+        {
+            files: ['*.js', '*.jsx'],
+            options: {
+                parser: 'babel',
+            },
+        },
+        {
+            files: ['*.ts', '*.tsx'],
+            options: {
+                parser: 'typescript',
+            },
+        },
+        {
+            files: ['*.json', '.*rc', '.yaml', '.yml'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
   return <div>123</div>;
 }
 
-console.log();
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
